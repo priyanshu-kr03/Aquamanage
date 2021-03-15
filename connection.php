98d8df6a0d8e73239c5928ac10a22dfa0164a0cb
KAPIL INANIA <67285213+newkapil@users.noreply.github.com>
2021-03-15 22:04:10 +1400

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-   // error_reporting(0);
-      $hostname="localhost";
-      $user="root";
-      $password="";
-      $dbname="jaljeevika";
-     $con= mysqli_connect($hostname,$user,$password,$dbname);
-   ?>
